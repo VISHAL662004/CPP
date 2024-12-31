099f65fe73cf6c4fa5f93969244de061f553e1a4
Vishal Kumar <139346808+VISHAL662004@users.noreply.github.com>
2024-12-31 11:02:03 +0530

    code

--- 11. Recursion/5_recurrsion_in_array.cpp
+++ 11. Recursion/5_recurrsion_in_array.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
-void print(int arr, int idx){
-    cout<<
+void print(int *arr, int idx, int length){
+   if(idx == length)    return;
+   if(length ==0) return;
+   cout<<arr[idx]<<endl;
+   print(arr,idx+1,length);
 }
 int main(){
     int arr[5] = {1,2,3,4,5};
-    print(arr,5);
-    retur 0;
+    print(arr,0,5);
+    return 0;
 }
